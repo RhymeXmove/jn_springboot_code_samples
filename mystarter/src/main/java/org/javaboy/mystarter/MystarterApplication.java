d9918dcb333208354a25254463e4ff587a0c30a7
RhymeXmove <61677430+RhymeXmove@users.noreply.github.com>
2021-02-19 17:53:38 +0800

    6 自定义springboot中的starter 2021-0219-1751

---
+++ mystarter/src/main/java/org/javaboy/mystarter/MystarterApplication.java
@@ -0,0 +1,24 @@
+package org.javaboy.mystarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigurationExcludeFilter;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.TypeExcludeFilter;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan(excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.CUSTOM, classes = TypeExcludeFilter.class),
+        @ComponentScan.Filter(type = FilterType.CUSTOM, classes = AutoConfigurationExcludeFilter.class)
+}
+)
+public class MystarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MystarterApplication.class, args);
+    }
+
+}
